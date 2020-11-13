2c175ba48889a0072c042e4351b7112b5d1bd5a1
Alex Francisco <alex2007f@gmail.com>
2020-11-13 17:41:23 -0300

    finalizando...

--- script.js
+++ script.js
@@ -4,19 +4,42 @@ function contar() {
     var passoCont = parseInt(document.getElementById('passoCont').value)
     var divCont = document.getElementById('contagem')
     var divBgNum = document.createElement('div')
+    divBgNum.classList.add('teste')
+    var btnContar = document.getElementById('btnContar')
 
-    if(iniCont.length == 0 || fimCont.length == 0 || passoCont.length == 0) {
+    console.log(iniCont.length)
+
+    if (document.getElementById('iniCont').value == "" || document.getElementById('fimCont').value == "" || passoCont == 0) {
         window.alert('ERRO!')
+    } else {
+        if (document.getElementById('passoCont').value == "") {
+            passoCont = 1
+        }
+        if ($('.teste')[0]) {
+            document.location.reload()
+            document.getElementsByClassName('teste').remove()
+            document.location.reload()
+        } else {
+            if (iniCont < fimCont) {
+                while (iniCont <= fimCont) {
+                    divBgNum.innerHTML = iniCont
+                    divCont.appendChild(divBgNum.cloneNode(true))
+                    divBgNum.innerHTML = '>'
+                    divCont.appendChild(divBgNum.cloneNode(true))   
+                    iniCont += passoCont
+                }
+            } else {
+                while (iniCont >= fimCont) {
+                    divBgNum.innerHTML = iniCont
+                    divCont.appendChild(divBgNum.cloneNode(true))
+                    divBgNum.innerHTML = '>'
+                    divCont.appendChild(divBgNum.cloneNode(true))   
+                    iniCont -= passoCont
+                }
+            } 
+            divBgNum.innerHTML = '&#127937'
+            divCont.appendChild(divBgNum.cloneNode(true))
+            btnContar.innerText = 'Limpar para inserir novo  valores'
+        }
     }
-     
-    while (iniCont <= fimCont) {
-        divBgNum.innerHTML = iniCont
-        divCont.appendChild(divBgNum.cloneNode(true))
-        divBgNum.innerHTML = '>'
-        divCont.appendChild(divBgNum.cloneNode(true))   
-        iniCont += passoCont
-    }
-    divBgNum.innerHTML = '&#127937'
-    divCont.appendChild(divBgNum.cloneNode(true))  
-    
 }
